633f9a430f3afc4bb7888c0f3b0e09022fcc9970
OfficialParsaNaderi <parsanaderi1387.programmer@gmail.com>
2025-11-05 21:02:40 +0330

    ToDo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const externals = config.externals || [];
+      config.externals = Array.isArray(externals)
+        ? [...externals, "better-sqlite3"]
+        : externals;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
